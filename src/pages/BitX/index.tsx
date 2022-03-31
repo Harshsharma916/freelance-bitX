4970e6c379735906e1c43a2801f43a27246d9c8e
Viper-M <vipersharma.88@gmail.com>
2022-03-31 21:25:52 +0530

    Fixed BITX

--- src/pages/BitX/index.tsx
+++ src/pages/BitX/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './bitx.scss';
 import bgVector from '../../assets/img/bgVector.png';
 import down from '../../assets/img/down.png';
